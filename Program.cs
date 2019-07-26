159c39707ae7af99ab093f5087e049a5f99da602
HENRIQUE RODRIGUES DA CUNHA <henrique.rcunha1@senacsp.edu.br>
2019-07-26 12:24:32 -0300

    Vetor5

--- Program.cs
+++ Program.cs
@@ -6,7 +6,37 @@ namespace Vetor5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numeros = {12,2,5,89,36,58,21};
+            int j = 89;
+            int k;
+            for(k = 0; k < 6; k++)
+
+            {
+
+            if (numeros[k] ==j)
+
+            {
+                Console.WriteLine(j);
+            }
+        
+            }
+
+            int numeros = {12,25,89,36,47,98,25};
+            int k = 7;
+            int i,p;
+            i = 0;
+
+            for (p = 0; p < k;++)
+            {
+                if(numeros[p] != 36)
+                {
+                    numeros[i++] = numeros[p];
+                }
+            }
+            n = n -1;
+            for(int x = 0; x < numeros.Length-1; x++){
+            Console.WriteLine(numeros[x]);
+            }
         }
     }
 }
